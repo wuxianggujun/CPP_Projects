5157353d2fcae3118a1bdc2a10072ef80e5e726e
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2023-07-09 00:39:00 +0800

    顶点数组

--- ChernoOpenGL/src/main.cpp
+++ ChernoOpenGL/src/main.cpp
@@ -137,6 +137,11 @@ int main() {
             2, 3, 0
     };
 
+    unsigned int vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+
     unsigned int buffer;
     //定义顶点缓冲区
     glGenBuffers(1, &buffer);
@@ -144,7 +149,7 @@ int main() {
     glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, nullptr);
 
 
     //定义索引缓冲区
@@ -163,6 +168,11 @@ int main() {
     ASSERT(location != -1);
     glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f);
 
+    glBindVertexArray(0);
+    glUseProgram(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
 
     float r = 0.0f;
     float increment = 0.05f;
@@ -171,7 +181,12 @@ int main() {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUseProgram(shader);
         glUniform4f(location, r, 0.3f, 0.8f, 1.0f);
+
+        glBindVertexArray(vao);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         if (r > 1.0f) {
