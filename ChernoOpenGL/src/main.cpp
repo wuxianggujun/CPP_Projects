cd92f34028bc832470ab777da693db34fe49d755
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2023-07-14 21:19:25 +0800

    修改添加imgui

--- ChernoOpenGL/src/main.cpp
+++ ChernoOpenGL/src/main.cpp
@@ -15,6 +15,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <imgui.h>
 #include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
 
 int main() {
@@ -96,6 +97,20 @@ int main() {
         Renderer renderer;
 
         ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO(); (void)io;
+        io.FontGlobalScale = 2.5f;
+
+        const char *glsl_version = "#version 150";
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init(glsl_version);
+
+        ImGui::StyleColorsDark();
+
+        // Our state
+        bool show_demo_window = true;
+        bool show_another_window = false;
+        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
 
 
         float r = 0.0f;
@@ -104,6 +119,13 @@ int main() {
         while (!glfwWindowShouldClose(window)) {
 
             renderer.Clear();
+
+            // Start the Dear ImGui frame
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+
 
             shader.Bind();
             //shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
@@ -119,6 +141,35 @@ int main() {
 
             r += increment;
 
+
+            {
+                static float f = 0.0f;
+                static int counter = 0;
+
+                ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+                ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+                ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+                ImGui::Checkbox("Another Window", &show_another_window);
+
+                ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+                ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+                if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                    counter++;
+                ImGui::SameLine();
+                ImGui::Text("counter = %d", counter);
+
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+                ImGui::End();
+            }
+
+
+
+
+            ImGui::Render();
+
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
 
@@ -126,6 +177,12 @@ int main() {
             glfwPollEvents();
         }
     }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
     glfwTerminate();
     return 0;
 }
