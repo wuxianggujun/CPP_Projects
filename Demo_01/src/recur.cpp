b383654743ca435da8e71b3635507854eea8dc4c
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2023-07-20 20:47:59 +0800

    添加例子

--- Demo_01/src/recur.cpp
+++ Demo_01/src/recur.cpp
@@ -1,3 +1,18 @@
 //
 // Created by WuXiangGuJun on 2023/7/16.
 //
+#include <iostream>
+
+void countdown(int n);
+
+int main() {
+    countdown(4);
+}
+
+void countdown(int n) {
+    using namespace std;
+    cout << "Counting down..." << n << " : " << &n << endl;
+    if (n > 0)
+        countdown(n - 1);
+    cout << n << ": Kaboom;\n";
+}
